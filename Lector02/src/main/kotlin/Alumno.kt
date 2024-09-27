acca4925b3a673302b4a79d59038b37391c1b93c
aaracan246 <acalberto@iesrafaelalberti.es>
2024-09-27 14:40:47 +0200

    base

---
+++ Lector02/src/main/kotlin/Alumno.kt
@@ -0,0 +1,12 @@
+package org.example
+
+data class Alumno(val apellidos: String,
+                  val nombre: String,
+                  val asistencia: Double,
+                  val parcial1: Double,
+                  val parcial2: Double,
+                  val ordinario1: Double?,
+                  val ordinario2: Double?,
+                  val practicas: Double?,
+                  val ordinarioPracticas: Double,
+                  val notaFinal: Double = 0.0)
---
+++ Lector02/src/main/kotlin/GestorFich.kt
@@ -0,0 +1,10 @@
+package org.example
+
+import java.io.File
+
+class GestorFich {
+
+    fun FichReader(file: File): List<Map<String, Double>>{
+
+    }
+}
